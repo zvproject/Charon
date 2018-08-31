075440203f625e0dd59c2de868878d4d7f9b640e
ZeroVash <jfv.contact@yahoo.com>
2018-08-31 01:57:10 -0400

    zvweb-002 project short description

--- app/functions.py
+++ app/functions.py
@@ -1,4 +1,6 @@
 from itertools import islice
+import glob
+import os
 
 def get_list_of_footer_social_links(path):
 	source_list = []
@@ -16,3 +18,24 @@ def get_list_of_footer_social_links(path):
 			n = len(line_source)
 			source_list.append(source)
 	return source_list
+
+def get_projects(path):
+	project_list = []
+	file_names = glob.glob(os.path.join(path,"project_*"))
+	for file_name in file_names:
+		with open(file_name, "r") as project_file:
+			project_info = {}
+			for line in project_file:
+				if ':' in line:
+					(key, val) = line.split(':')
+					project_info[str(key)] = val.strip()
+				else:
+					project_info[str(key)] += line.strip()
+		project_list.append(project_info)
+	return project_list
+
+def get_text(path):
+	with open(path,"r") as file_with_text:
+		for line in file_with_text:
+			text += line
+	return text 
--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from flask import render_template, url_for
 from app import app, APP_SOURCE
-from app.functions import get_list_of_footer_social_links
+from app.functions import get_list_of_footer_social_links, get_projects
 import os
 
 footer_list = get_list_of_footer_social_links(os.path.join(APP_SOURCE, "footer_links.txt"))
@@ -14,8 +14,30 @@ def index():
 
 @app.route('/projects')
 def projects():
-
+	get_projects(APP_SOURCE)
 	return render_template("projects.html", title="Projects", links=footer_list)
+
+@app.route('/portfolio')
+def portfolio():
+	navigation = ['Art','Web Design','Animation','Coding']
+	return render_template("main_menu.html", title="Portfolio", links=footer_list, menu=navigation)
+
+@app.route('/art')
+def art():
+	return render_template("art.html", title="Art", links=footer_list)
+
+@app.route('/web')
+def web():
+	return render_template("web.html", title="Web Design", links=footer_list)
+
+@app.route('/animation')
+def animation():
+	return render_template("animation.html", title="Animation", links=footer_list)
+
+@app.route('/coding')
+def coding():
+	return render_template("coding.html", title="Coding", links=footer_list)
+
 
 @app.errorhandler(404)
 def page_not_found(error):
