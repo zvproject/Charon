12ef03b80af64d40f09ff59af06d7bfcdac58252
Zero Vash <22164514+zvproject@users.noreply.github.com>
2018-11-14 21:24:44 -0500

    Zvweb 002 (#2)
    * zvweb-001 Desktop /index main-menu ready
    
    * zvweb-001 fix footer right side
    
    * zvweb-002 project short description
    
    * zvweb-002 project page, css finish, without js, not tested
    
    * zvweb-002 testing and change limits
    
    * zvweb-002 projects
    
    * zvweb-002 projects

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from flask import render_template, url_for
-from app import app, APP_SOURCE
-from app.functions import get_list_of_footer_social_links
+from app import app, APP_SOURCE, APP_STATIC
+from app.functions import get_list_of_footer_social_links, get_projects, get_art
 import os
 
 footer_list = get_list_of_footer_social_links(os.path.join(APP_SOURCE, "footer_links.txt"))
@@ -14,8 +14,38 @@ def index():
 
 @app.route('/projects')
 def projects():
+	projects = get_projects(APP_SOURCE)
+	return render_template("projects.html", title="Projects", links=footer_list, list=projects)
 
-	return render_template("projects.html", title="Projects", links=footer_list)
+@app.route('/portfolio')
+def portfolio():
+	navigation = ['Art','Web Design','Animation','Coding']
+	return render_template("main_menu.html", title="Portfolio", links=footer_list, menu=navigation)
+
+@app.route('/art')
+def art():
+	art = get_art(APP_STATIC+'/img/art')
+	print(art)
+	return render_template("projects.html", title="Art", links=footer_list, list=art)
+
+@app.route('/web')
+def web():
+	return render_template("web.html", title="Web Design", links=footer_list)
+
+@app.route('/animation')
+def animation():
+	return render_template("animation.html", title="Animation", links=footer_list)
+
+@app.route('/coding')
+def coding():
+	return render_template("coding.html", title="Coding", links=footer_list)
+
+@app.route('/team')
+def team():
+	#team_list = get_team_list()
+	project_cryteria = None
+	role = None
+	return render_template("team.html", title="Team", links=footer_list)
 
 @app.errorhandler(404)
 def page_not_found(error):
